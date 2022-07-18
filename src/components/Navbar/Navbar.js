9919cd9c5343b30f6e46aeda88032023b636b39e
whid-bb <whid11s50@gmail.com>
2022-07-18 16:43:45 +0200

    create empty navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+// import { NavLink, Link } from 'react-router-dom';
+import style from './navbar.module.css';
+// import logo from '../../img/logo.png'
+
+const Navbar = () => (
+  <nav className={style.topNav}>
+    Navbar
+  </nav>
+);
+
+export default Navbar;
