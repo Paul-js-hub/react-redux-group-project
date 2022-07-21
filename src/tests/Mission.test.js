e16deec05bb4fba95819a16a58c0b93a2ac1cf0c
Paul-js-hub <hesponpoisson@gmail.com>
2022-07-21 17:34:57 +0300

    Fix linter errors

--- src/tests/Mission.test.js
+++ src/tests/Mission.test.js
@@ -1,18 +1,18 @@
 import renderer from 'react-test-renderer';
-import Mission from '../components/Missions/Mission';
-import store from '../redux/configureStore'
 import { Provider } from 'react-redux';
+import Mission from '../components/Missions/Mission';
+import store from '../redux/configureStore';
 
 test('it returns a mission component', () => {
-    const tree = renderer.create(
-        <Provider store={store}>
-            <Mission 
-            id='1'
-            name='name'
-            description='description'
-            reserved={false}
-            />
-        </Provider>
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+  const tree = renderer.create(
+    <Provider store={store}>
+      <Mission
+        id="1"
+        name="name"
+        description="description"
+        reserved={false}
+      />
+    </Provider>,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- src/tests/Missions.test.js
+++ src/tests/Missions.test.js
@@ -1,13 +1,13 @@
 import renderer from 'react-test-renderer';
-import Missions from '../components/Missions/Missions';
-import store from '../redux/configureStore'
 import { Provider } from 'react-redux';
+import Missions from '../components/Missions/Missions';
+import store from '../redux/configureStore';
 
 test('it returns a missions component', () => {
-    const tree = renderer.create(
-        <Provider store={store}>
-            <Missions />
-        </Provider>
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+  const tree = renderer.create(
+    <Provider store={store}>
+      <Missions />
+    </Provider>,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
