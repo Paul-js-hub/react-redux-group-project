692237568b2b16b7ecf39ad0e620d407f4b94459
whid-bb <whid11s50@gmail.com>
2022-07-21 11:05:38 +0200

    add navbar styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,32 @@
+/* eslint-disable import/no-named-default */
 import { NavLink } from 'react-router-dom';
+import { default as BootstrapNavbar } from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import logo from '../../img/logo.png';
+import style from './navbar.module.css';
 
 const Navbar = () => (
-  <nav>
-    <NavLink to="/">Rockets</NavLink>
-    <NavLink to="/missions">Missions</NavLink>
-    <NavLink to="/profile">My Profile</NavLink>
-  </nav>
+  <BootstrapNavbar bg="white" variant="light" className={style.navContainer} expand="lg">
+    <Container fluid>
+      <BootstrapNavbar.Brand href="#home" className="me-auto">
+        <img
+          alt=""
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />
+        <h3 className="d-inline">React Bootstrap</h3>
+      </BootstrapNavbar.Brand>
+      <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+      <BootstrapNavbar.Collapse className={style.linkContainer}>
+        <NavLink className={({ isActive }) => (isActive ? style.active : '')} to="/">Rockets</NavLink>
+        <NavLink className={({ isActive }) => (isActive ? style.active : '')} to="/missions">Missions</NavLink>
+        <NavLink className={({ isActive }) => (isActive ? style.active : '')} to="/profile">My Profile</NavLink>
+      </BootstrapNavbar.Collapse>
+    </Container>
+  </BootstrapNavbar>
 );
 
 export default Navbar;
