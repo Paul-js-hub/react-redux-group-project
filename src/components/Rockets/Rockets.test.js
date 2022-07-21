8875915a63a4e1899bf74fe6d088013163377c93
whid-bb <whid11s50@gmail.com>
2022-07-21 11:25:26 +0200

    add Rockets component test

---
+++ src/components/Rockets/Rockets.test.js
@@ -0,0 +1,24 @@
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../../redux/configureStore';
+import Rockets from './Rockets';
+
+describe('Rockets test', () => {
+  it('Rockets render correctly', () => {
+    const RocketsPage = renderer.create(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>
+    ).toJSON();
+    expect(RocketsPage).toMatchSnapshot();
+  });
+
+  test('Render Rockets component', () => {
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>
+    );
+  });
+});
